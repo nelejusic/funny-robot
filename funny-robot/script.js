012ec3547ce3ac71c85184ca7a456d42884f2121
nelethemamba998 <72813469+nelethemamba998@users.noreply.github.com>
2021-11-19 01:03:29 +0100

    finished, added function for fetching jokes, than transformed the data in an async function and than passed the data in the tellme funciton that plays the audio, added hidden atribute to the html audio element.

--- funny-robot/script.js
+++ funny-robot/script.js
@@ -102,16 +102,39 @@ const VoiceRSS = {
     throw "The browser does not support HTTP request";
   },
 };
-function test() {
+function toggleButton(){
+    button.disabled = !button.disabled;
+}
+function tellMe(joke) {
+  const jokeString = joke.trim().replace(/ /g, "%20");
   VoiceRSS.speech({
     key: "06363e76d1b74e8ab204b6bc5349c3b3",
-    src: "Hello, world!",
+    src: jokeString,
     hl: "en-us",
-    v: "Linda",
     r: 0,
     c: "mp3",
     f: "44khz_16bit_stereo",
     ssml: false,
   });
 }
-test();
+async function getJokes() {
+  let joke = "";
+  const apiUrl =
+    "https://v2.jokeapi.dev/joke/Programming,Miscellaneous,Dark,Pun,Spooky?blacklistFlags=religious,political,racist,sexist,explicit";
+  try {
+    const res = await fetch(apiUrl);
+    const data = await res.json();
+    if (data.setup) {
+      joke = `${data.setup} ... ${data.delivery}`;
+    } else {
+      joke = data.joke;
+    }
+    tellMe(joke);
+    toggleButton()
+    console.log(joke);
+  } catch (error) {
+    console.log("somebody messed up", error);
+  }
+}
+button.addEventListener("click", getJokes);
+audioElement.addEventListener('ended',toggleButton);
